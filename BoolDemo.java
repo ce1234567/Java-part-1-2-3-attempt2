65bb15a741753114f1709e34bfac2bd6bfc01f65
ce1234567 <116823071+ce1234567@users.noreply.github.com>
2025-03-19 22:05:21 +0300

    Add files via upload

---
+++ BoolDemo.java
@@ -0,0 +1,9 @@
+public class BoolDemo {
+    public static void main (String[]args) {
+        boolean b;
+        b= false;
+        System.out.println ("the value is  "+b);
+        if (b) System.out.println ("The instruction has to be executed");
+    }
+
+}
